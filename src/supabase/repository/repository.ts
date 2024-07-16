fb4578ca8c2c2aa2271e59b07ee8728e53655ff3
cleiton.ti <cleiton.ti@outlook.com>
2024-07-16 17:39:06 -0300

    feat: supa repository

---
+++ src/supabase/repository/repository.ts
@@ -0,0 +1,28 @@
+import { SupabaseClient } from "@supabase/supabase-js";
+import { createClient } from "@supabaseutils/utils/server";
+
+export default class SupabaseRepository {
+  protected supabase: SupabaseClient;
+  protected TABLE: string;
+
+  constructor(table: string, supabase?: SupabaseClient) {
+    this.TABLE = table;
+    this.supabase = supabase ? supabase : createClient();
+  }
+
+  public save(entity: any) {
+    return this.supabase.from(this.TABLE).upsert(entity).select();
+  }
+
+  public findById(id: string) {
+    return this.supabase.from(this.TABLE).select().eq("id", id);
+  }
+
+  public findBy(query: any[]) {
+    const builder = this.supabase.from(this.TABLE).select();
+
+    Object.keys(query).forEach((key) => {
+      builder.eq(key, query[key]);
+    });
+  }
+}
