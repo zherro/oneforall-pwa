af040c08029d48957f3164924936b6ad51206fab
cleiton.ti <cleiton.ti@outlook.com>
2024-07-19 17:56:08 -0300

    fix: pwa meta tag ativation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,41 +1,25 @@
 // components/layout.tsx
 import { ReactNode } from "react";
 import Head from "next/head";
+import { Metadata } from "next";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
+export const metadata: Metadata = {
+  title: "Next.js PWA Example",
+  description: "An example of a PWA with Next.js",
+  keywords: "PWA, Next.js, Example",
+  viewport:
+    "width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no",
+  themeColor: "#317EFB",
+  manifest: "/manifest.json",
+};
+
 const Layout = ({ children }: LayoutProps) => {
   return (
     <html>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-        <title>Next.js PWA Example</title>
-
-        <link rel="manifest" href="/manifest.json" />
-        <link
-          href="/icons/favicon-16x16.png"
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-        />
-        <link
-          href="/icons/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-        />
-        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
-      </Head>
       <body>
         <main>{children}</main>
       </body>
