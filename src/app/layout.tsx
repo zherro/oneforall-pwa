0e756a9811bb6f15b57ce4d7a47532fe5221755c
cleiton.ti <cleiton.ti@outlook.com>
2024-07-19 22:03:16 -0300

    chore: pwa best pratices

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 // components/layout.tsx
 import { ReactNode } from "react";
-import Head from "next/head";
 import { Metadata } from "next";
 
 interface LayoutProps {
@@ -15,12 +14,19 @@ export const metadata: Metadata = {
     "width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no",
   themeColor: "#317EFB",
   manifest: "/manifest.json",
+
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/icon-128x128.png" },
+    { url: "/icons/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+    { url: "/icons/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+  ],
 };
 
 const Layout = ({ children }: LayoutProps) => {
   return (
     <html>
-      <body>
+      <body suppressHydrationWarning={true}>
         <main>{children}</main>
       </body>
     </html>
