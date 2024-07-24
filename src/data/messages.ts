3ce73af8789bb606a322d7e0321ec87527338a7f
cleiton.ti <cleiton.ti@outlook.com>
2024-07-24 15:48:18 -0300

    chore: format

--- src/data/messages.ts
+++ src/data/messages.ts
@@ -5,11 +5,16 @@ const MESSAGES = {
       INVALID_EMAIL: "Email inválido",
       PASSWORD_MIN_LENGHT: "A senha deve ter ao menos 6 carateres",
       PASSWORD_MAX_LENGHT: "A senha deve ter no máximo 16 carateres",
+      PASSWORD_SHOULD_EQUALS: "A senhas devem ser iguais",
+      PASSWORD_CONFIRM_IS_REQUIRED: "Para continuar, digite a senha novamente",
+      PUT_VERIFICARION_CODE: "Informe o código de verificação",
+      PUT_LOGIN: "Informe seu login",
     },
   },
 
   // USER MESSAGES
-  INVALID_CREDENTIALS_TOKE: "A verificação expirou ou é invalida! Solicite uma nova recuperação de senha, para receber um novo token."
+  INVALID_CREDENTIALS_TOKE:
+    "A verificação expirou ou é invalida! Solicite uma nova recuperação de senha, para receber um novo token.",
 };
 
 export default MESSAGES;
