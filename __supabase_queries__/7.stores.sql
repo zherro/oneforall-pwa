2c5c6a688754671571115838e777833e816e6788
cleiton <cleiton@cleiton.oliveira.ext>
2024-07-30 09:28:49 -0300

    sql: store tables

--- __supabase_queries__/7.stores.sql
+++ __supabase_queries__/7.stores.sql
@@ -1,4 +1,37 @@
 
+-- public.stores definition
+
+-- Drop table
+
+-- DROP TABLE public.stores;
+
+CREATE TABLE public.stores (
+	id uuid DEFAULT gen_random_uuid() NOT NULL,
+	created_at timestamp DEFAULT now() NOT NULL,
+	updated_at timestamp NULL,
+	status varchar DEFAULT 'A'::character varying NULL,
+	deleted bool DEFAULT false NULL,
+	"name" varchar NULL,
+	market_type varchar NULL,
+	person_type varchar NULL,
+	document_number varchar NOT NULL,
+	document_number_owner varchar NULL,
+	user_id uuid DEFAULT auth.uid() NOT NULL,
+	tenant_id uuid DEFAULT gen_random_uuid() NOT NULL,
+	CONSTRAINT stores_pkey PRIMARY KEY (id)
+);
+
+-- Table Triggers
+
+create trigger auto_insert_tenant_store_owner after
+insert
+    on
+    public.stores for each row execute function auto_insert_tenant();
+
+
+
+alter table "stores" enable row level security;
+
 
 CREATE OR REPLACE FUNCTION public.auto_insert_tenant()
  RETURNS trigger
@@ -14,6 +47,6 @@ END;
 $function$
 ;
 
-
-CREATE TRIGGER auto_insert_tenant_store_owner AFTER INSERT ON public.stores  
-FOR EACH ROW  EXECUTE PROCEDURE auto_insert_tenant();
+create policy "Can update own store" on "public"."stores" as permissive for update to public using ((( SELECT uid() AS uid) = user_id));
+create policy "Enable insert for users based on user_id" on "public"."stores" as permissive for insert to public with check ((( SELECT uid() AS uid) = user_id));
+create policy "Public select stores" on "public"."stores" as permissive for select to anon, authenticated using (true);
