0f5774c915e0c8eac646a76c7f8a3ab5d3f0e1d0
cleiton <cleiton@cleiton.oliveira.ext>
2024-08-21 09:13:51 -0300

    chore: sys data

---
+++ __supabase_queries__/0.sys_parameter-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.sys_parameter
+(id, created_at, status, deleted, "name", string_value, number_value, date_value, bool_value, config, description)
+VALUES(1, '2024-08-21 04:31:18.581', 'A', false, 'ONBOARD_STEPS', NULL, NULL, NULL, NULL, '[{"link": "/complete-your-profile", "title": "Completar perfil", "completed": false, "completedAt": null, "description": "Complete seu perfil"}, {"link": "/minhaconta/stores/minha-loja", "title": "Crie sua primeira loja", "completed": false, "completedAt": null, "description": "Configure sua loja para desbloquear novas funcionalidades!"}, {"link": "/minhaconta/stores/minha-loja", "title": "Comece a gerencia sua loja", "completed": false, "completedAt": null, "description": "Depois de criar uma loja, começe a gerencia-la através do menu MEU NEGÓCIO."}]'::jsonb, NULL);
+
